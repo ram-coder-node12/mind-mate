ee7e36161ef94e381bf393474251baf70bbf5c70
ram-coder-node12 <ram.bezawada2007@gmail.com>
2026-04-20 15:47:07 +0530

    Fix GitHub Pages base path

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const Analytics = lazy(() => import('./pages/Analytics'));
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <AppDataProvider>
           <Routes>
